d8c436f3b8427c6cba821bca127a6894f6df5604
Filippov Konstantin <fkv.katafey@yandex.ru>
2024-06-03 02:54:00 +0400

    Добавил картинку загрузки приложения, настроил констрейнты для всех добавленных объектов

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -1,17 +1,29 @@
 import UIKit
 
 final class MovieQuizViewController: UIViewController {
+    
     // MARK: - Lifecycle
     
     @IBOutlet weak var buttonYes: UIButton!
     @IBOutlet weak var buttonNo: UIButton!
+    @IBOutlet weak var questions: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         buttonYes.layer.cornerRadius = 15
+        buttonYes.titleLabel?.font = UIFont(name: "YS Display-Medium", size: 20)
+        buttonYes.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        buttonYes.setImage(UIImage(systemName: "checkmark.circle"), for: .normal)
+        buttonYes.tintColor = .ypGreen
         buttonNo.layer.cornerRadius = 15
-            
+        buttonNo.titleLabel?.font = UIFont(name: "YS Display-Medium", size: 20)
+        buttonNo.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        buttonNo.setImage(UIImage(systemName: "hand.raised"), for: .normal)
+        buttonNo.tintColor = .ypRed
+        
+        questions.font = UIFont(name: "YS Display-Medium", size: 20)
+
     }
     
     @IBAction func buttonYes(_ sender: Any) {
