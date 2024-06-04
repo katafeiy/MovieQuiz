fc183055ed90123cf466784abfd35f9a9000313e
Filippov Konstantin <fkv.katafey@yandex.ru>
2024-06-05 02:41:11 +0400

    Окончательно настроил расположение все элементов на экране, а так же перенёс в код установку необходтимых шрифтов и размеров

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -4,35 +4,51 @@ final class MovieQuizViewController: UIViewController {
     
     // MARK: - Lifecycle
     
-    @IBOutlet weak var buttonYes: UIButton!
-    @IBOutlet weak var buttonNo: UIButton!
-    @IBOutlet weak var questions: UILabel!
+    @IBOutlet private weak var buttonYes: UIButton!
+    @IBOutlet private weak var buttonNo: UIButton!
+    @IBOutlet private weak var questionsTitleLabel: UILabel!
+    @IBOutlet private weak var IndexLabel: UILabel!
+    @IBOutlet private weak var questionLabel: UILabel!
+    @IBOutlet private weak var previewImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupFonts()
+        setupUI()
+        
+    }
+    
+    @IBAction private func buttonYes(_ sender: Any) {
+    }
+    
+    @IBAction private func buttonNo(_ sender: Any) {
+    }
+    
+    private func setupFonts() {
+        
+        buttonYes.titleLabel?.font = UIFont(name: "YSDisplay-Medium", size: 20)
+        buttonNo.titleLabel?.font = UIFont(name: "YSDisplay-Medium", size: 20)
+        questionsTitleLabel.font = UIFont(name: "YSDisplay-Medium", size: 20)
+        IndexLabel.font = UIFont(name: "YSDisplay-Medium", size: 20)
+        questionLabel.font = UIFont(name: "YSDisplay-Bold", size: 23)
+
+    }
+    private func setupUI() {
+        
         buttonYes.layer.cornerRadius = 15
-        buttonYes.titleLabel?.font = UIFont(name: "YS Display-Medium", size: 20)
-        buttonYes.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
         buttonYes.setImage(UIImage(systemName: "checkmark.circle"), for: .normal)
         buttonYes.tintColor = .ypGreen
         buttonNo.layer.cornerRadius = 15
-        buttonNo.titleLabel?.font = UIFont(name: "YS Display-Medium", size: 20)
-        buttonNo.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
         buttonNo.setImage(UIImage(systemName: "hand.raised"), for: .normal)
         buttonNo.tintColor = .ypRed
+        previewImage.layer.cornerRadius = 20
         
-        questions.font = UIFont(name: "YS Display-Medium", size: 20)
-
-    }
-    
-    @IBAction func buttonYes(_ sender: Any) {
-    }
-    
-    @IBAction func buttonNo(_ sender: Any) {
     }
     
 }
+
+
 
 
 
@@ -100,4 +116,4 @@ final class MovieQuizViewController: UIViewController {
  Настоящий рейтинг: 5,8
  Вопрос: Рейтинг этого фильма больше чем 6?
  Ответ: НЕТ
-*/
+ */
